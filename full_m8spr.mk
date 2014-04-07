e6d397f2b1e87509539dbda303fe3cf704fd1f21
Richard Ross <toastcfh@gmail.com>
2014-04-07 16:32:16 -0400

    m8spr: move moar of the things to msm8974-common
    - move init scripts and fstab to msm8974 common
    - fix inherit of msm8974 common
    - remove prebuilt dtb file
    
    Change-Id: I0855cf29c08c3a5f97ff0198b45abbe533578c35

--- full_m8spr.mk
+++ full_m8spr.mk
@@ -24,7 +24,10 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from m8 device
+# Inherit from msm8974-common
+$(call inherit-product, device/htc/msm8974-common/msm8974.mk)
+
+# Inherit from m8spr device next for overrides
 $(call inherit-product, device/htc/m8spr/device.mk)
 
 # Set those variables here to overwrite the inherited values.
