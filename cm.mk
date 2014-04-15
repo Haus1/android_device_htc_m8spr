4a2c74d851dd25620beef2fcc97c0195570bc480
John Kirk <jkirk777@gmail.com>
2014-04-15 11:37:48 -0600

    Update build ID fingerprint

--- cm.mk
+++ cm.mk
@@ -1,8 +1,8 @@
 $(call inherit-product, device/htc/m8spr/full_m8spr.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-        BUILD_FINGERPRINT="htc/sprint_wwe/htc_m8whl:4.4.2/KOT49H/318427.17:user/release-keys" \
-        PRIVATE_BUILD_DESC="1.12.651.17 CL318427 release-keys"
+        BUILD_FINGERPRINT="htc/sprint_wwe/htc_m8whl:4.4.2/KOT49H/326598.8:user/release-keys" \
+        PRIVATE_BUILD_DESC="1.54.651.8 CL326598 release-keys"
 
 PRODUCT_NAME := cm_m8spr
 PRODUCT_DEVICE := m8spr
